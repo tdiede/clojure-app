b54f376ff91bfe3fc6212f1dfa1cc2f8fc0e9df5
therese <tdiede@gmail.com>
2018-04-07 15:22:10 -0700

    aot ahead of time capabilities

--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,6 +1,23 @@
-(ns clojure-simple-http.core)
+(ns clojure-simple-http.core
+  (:gen-class)
+  (:require [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-time
+  []
+  (let [response {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    (str (t/time-now))}]
+    response))
+
+(defroutes app
+  (GET "/" [] "<h1>Welcome to Therese's Docker container. This is it.</h1>")
+  (GET "/get-time" [] (get-time))
+  (route/not-found "<h1>Page not found</h1>"))
+
+
+(defn -main [& args]
+  (run-server app {:port 8080})
+  (println "Server started on port 8080"))
